6d8f98093b76bb9d472a636ecedb1db79f50e8e0
Shen <ihaiyon@gmail.com>
2025-06-15 13:14:19 +0800

    Disable default completion

--- cmd/root.go
+++ cmd/root.go
@@ -27,6 +27,9 @@ var rootCmd = &cobra.Command{
 }
 
 func Execute() {
+	// Disable completion
+	rootCmd.CompletionOptions.DisableDefaultCmd = true
+	// Execute root command
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
